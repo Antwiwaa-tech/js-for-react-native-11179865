baaa3c61bcb350fce242cd1e0f0c455fd56e289a
Antwiwaa <nanaafuaantwiwaa624@gmail.com>
2024-05-18 23:00:39 +0000

    added formatArrayString

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -3,19 +3,37 @@
 function processArray(arr) {
     return arr.map(num => {
         if (num % 2 === 0) {
-            // Even number: square it
+            
             return num**2;
         } else {
-            // Odd number: triple it
+    
             return num ** 3;
         }
     });
 }
 
-// Example usage:
+
 const numbers = [1, 2, 3, 4, 5, 6];
 const newNumbers = processArray(numbers);
-console.log(newNumbers); // Output: [3, 4, 9, 16, 15, 36]
+console.log(newNumbers); 
+
+function formatArrayStrings(word , arr){
+    return word.map((str, index) => {
+        const num = numbers[index];
+        return num % 2 === 0 ? str.toUpperCase() : str.toLowerCase();
+        }
+    );
+
+    
+}
+
+
+ const word = ["antwiwaa", "afua", "gladys", "apple", "app"];
+const formattedStrings = formatArrayStrings(word, newNumbers);
+console.log(formattedStrings);
+
+export const previous = word
+export const newWord = formattedStrings
 
   
 
